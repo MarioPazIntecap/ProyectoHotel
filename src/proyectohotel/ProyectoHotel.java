ba78b286fb0fd6dcb915f9b886d3ad67f9353955
UsuarioLocal <UsuarioLocal@DESKTOP-UVN83DS>
2022-09-02 21:25:19 -0600

    Se agrego el codigo para una tabla de multiplicar del 7.

--- src/proyectohotel/ProyectoHotel.java
+++ src/proyectohotel/ProyectoHotel.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectohotel;
 
-/**
- *
- * @author UsuarioLocal
- */
 public class ProyectoHotel {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hola GitHub");
+        int a = 7;
+        for(int i = 1;i<=10;i++){
+            System.out.println(i +" x "+ a + " = " + (a*i));
+        }
     }
     
 }
